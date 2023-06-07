4a50eed313dc414aeca64feb480e62cd64c19fb9
chaimalk <108950943+chaimalk@users.noreply.github.com>
2023-06-07 23:54:59 +0200

    Create test0.ads

---
+++ test0.ads
@@ -0,0 +1,13 @@
+{
+  "AdsController": {
+    "adNetwork": "applovin",
+    "admobBanner": "",
+    "admobInterstitial": "",
+    "applovinBanner": "ChangeThisforbanner",
+    "applovinInterstitial": "ChangeThisforInter",
+    "unityGameId": "",
+    "testMode": false,
+    "unityBanner": "Banner_Android",
+    "unityInterstitial": "Interstitial_Android"
+  }
+}
